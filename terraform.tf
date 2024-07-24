410a1aa83edf54c90dcc1e35651adb378db69cf5
chiptranz-int <69421097+chiptranz-int@users.noreply.github.com>
2024-07-24 15:49:55 -0500

    deleted terraform file

--- terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = "~> 1.4"
-
-  backend "s3" {
-    key    = "github-actions-iaac-apache/terraform.tfstate" # the directory/file.tfstate
-    bucket = "polyiabucket"             # the bucket
-    region = "us-east-1"             # the region
-  }
-}
