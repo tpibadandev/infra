2d54fc8cdb9c45b763a563ddec6565eea505c49d
chiptranz-int <69421097+chiptranz-int@users.noreply.github.com>
2024-07-26 12:57:30 -0500

    updated output.tf file

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 # Create an output for the public_dns of each EC2 instance and db endpoint
 output "db_endpoint" {
-  value = aws_db_instance.default.endpoint
+  value = aws_db_instance.db_instance.endpoint
 }
 output "ec2" {
   value = {
