2f3f65cb602b872166147c1f90d943e152907272
chiptranz-int <69421097+chiptranz-int@users.noreply.github.com>
2024-07-26 11:48:44 -0500

    updated output.tf file

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-# Create an output for the public_dns of each EC2 instance and db endpoint
-output "db_endpoint" {
-  value = aws_db_instance.default.endpoint
-}
-output "ec2" {
-  value = {
-    for name, this in aws_instance.ec2 : name => this.public_dns
-  }
-=======
 #Outputs
 output "web_server_public_ip" {
   description = "Public IP of Web Servers"
@@ -37,5 +27,4 @@ output "alb_public_url" {
 output "connect_to_database" {
   description = "Command to connect to database from EC2"
   value       = module.create_two_tier_aws.connect_to_database
->>>>>>> 0cf6799efc42bd0256466bff3b5b0ecfa2a604d7
 }
