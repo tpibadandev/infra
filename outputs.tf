e25d43159065c6264e4ce671a722de4f798a4d1f
chiptranz-int <69421097+chiptranz-int@users.noreply.github.com>
2024-07-26 12:19:36 -0500

    updated output.tf file

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,9 @@
-#Outputs
-output "web_server_public_ip" {
-  description = "Public IP of Web Servers"
-  value       = module.create_two_tier_aws.web_server_public_ip
+# Create an output for the public_dns of each EC2 instance and db endpoint
+output "db_endpoint" {
+  value = aws_db_instance.default.endpoint
 }
-
-output "ec2_ssh_access" {
-  description = "Remote Access to EC2"
-  value       = module.create_two_tier_aws.ec2_ssh_access
-}
-
-output "db_name" {
-  description = "Database Name"
-  value       = module.create_two_tier_aws.db_name
-}
-
-output "db_address" {
-  description = "The hostname of the RDS instance"
-  value       = module.create_two_tier_aws.db_address
-}
-
-output "alb_public_url" {
-  description = "Public URL for Application Load Balancer"
-  value       = module.create_two_tier_aws.alb_public_url
-}
-
-output "connect_to_database" {
-  description = "Command to connect to database from EC2"
-  value       = module.create_two_tier_aws.connect_to_database
+output "ec2" {
+  value = {
+    for name, this in aws_instance.ec2 : name => this.public_dns
+  }
 }
