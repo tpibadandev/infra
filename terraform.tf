a0e72e615873f97a106ef8d887ff4260fc2b3573
tpibadandev <thepolytechnicibadan0923@gmail.com>
2024-08-05 15:47:09 -0500

    Delete terraform.tf

--- terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = "~> 1.4"
-
-  backend "s3" {
-    key    = "github-actions-iaac-apache/terraform.tfstate" # the directory/file.tfstate
-    bucket = "polyiabucket"             # the bucket
-    region = "us-west-2"             # the region
-  }
-}
