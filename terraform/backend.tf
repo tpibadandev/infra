420c4711571998d8cf642e59b10c93de741e51f6
chiptranz-int <69421097+chiptranz-int@users.noreply.github.com>
2024-08-05 13:11:33 -0500

    updated complete project

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mycloudprojects"
+    bucket = "polyiacbucket"
     key    = "State-Files/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-west-2"
   }
 }
